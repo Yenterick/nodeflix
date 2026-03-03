ad4c96ff1580d99c49924a3eec17c6b443331342
Yenterick <yenterick@gmail.com>
2026-03-02 22:10:09 -0500

    Login container background color changed

--- client/assets/color/colorScheme.js
+++ client/assets/color/colorScheme.js
@@ -1,6 +1,7 @@
 // Object with the colors used in the client
 const colorScheme = {
     darkGreen: 'rgb(27, 33, 26)',
+    bgDarkGreen: 'rgb(14, 17, 14)',
     green: 'rgb(98, 129, 65)',
     lightGreen: 'rgb(139, 174, 102)',
     beige: 'rgb(235, 213, 171)'
