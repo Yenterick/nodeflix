97198ec9faffac324e857dec37f63427308c3f01
Yenterick <yenterick@gmail.com>
2026-03-21 13:28:55 -0500

    Associations updated

--- server/models/associations.js
+++ server/models/associations.js
@@ -1,6 +1,8 @@
 const { User } = require('./userModel');
 const { Profile } = require('./profileModel');
 const { ViewEvent } = require('./viewEventModel');
+const { ListEvent } = require('./listEventModel');
+const { InteractionEvent } = require('./interactionEventModel');
 
 // Configuring the 1:N relationship between user and profiles
 User.hasMany(Profile, {
@@ -30,6 +32,36 @@ ViewEvent.belongsTo(Profile, {
         name: 'profile_id',
         allowNull: false
     }
-})
+});
+
+// Configuring the 1:N relationship between profile and list events
+Profile.hasMany(ListEvent, {
+    foreignKey: {
+        name: 'profile_id'
+    },
+    as: 'list_events'
+});
+
+ListEvent.belongsTo(Profile, {
+    foreignKey: {
+        name: 'profile_id',
+        allowNull: false
+    }
+});
+
+// Configuring the 1:N relationship between profile and interaction events
+Profile.hasMany(InteractionEventEvent, {
+    foreignKey: {
+        name: 'profile_id'
+    },
+    as: 'interaction_events'
+});
+
+InteractionEvent.belongsTo(Profile, {
+    foreignKey: {
+        name: 'profile_id',
+        allowNull: false
+    }
+});
 
 module.exports = { User, Profile };
